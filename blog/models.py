c27ef8a8dce6fc4cbe3ca12a8a1707bc8fa9035d
“Rupesh” <rupesgpatidar@gmail.com>
2023-06-04 21:08:08 +0530

    Project for  local development server

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,12 @@ class Post(models.Model):
     #if related name was not passed then for reverse relation we have to use post_set
     tags=models.ManyToManyField(Tag)
     
+    def __str__(self):
+        return self.title
+        
+    
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post , on_delete=models.CASCADE , related_name = "comments")
